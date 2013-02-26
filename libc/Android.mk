e3909b471df46fcf8cf35096e754f8c1375dace0
Kejar31 <Kejar31@gmail.com>
2013-02-26 16:28:15 -0600

    derp

--- libc/Android.mk
+++ libc/Android.mk
@@ -397,7 +397,7 @@ endif
 # We have a special memcpy for A15 currently
 ifeq ($(TARGET_ARCH_VARIANT_CPU),cortex-a15)
 libc_common_src_files += arch-arm/bionic/memcpy-a15.S
-#else
+else
 libc_common_src_files += arch-arm/bionic/memcpy.S
 endif
 
